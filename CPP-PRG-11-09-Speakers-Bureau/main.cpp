7550ed706bc2a6fc4937d0abb20bab6c1693f565
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-08 17:45:49 -0500

    Prompt written.

--- CPP-PRG-11-09-Speakers-Bureau/main.cpp
+++ CPP-PRG-11-09-Speakers-Bureau/main.cpp
@@ -5,11 +5,24 @@
 //  Created by Keith Smith on 11/8/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that keeps track of a speaker's bureau. The program should use a
+//  structure to store the following data about a speaker:
+//
+//      Name
+//      Telephone Number
+//      Speaking Topic
+//      Fee Required
+//
+//  The program should use an array of at least 10 structures. It should let the user enter
+//  data into the array, change the contents of any element, and display all the data stored
+//  in the array. The program should have a menu-driven user interface.
+//
+//  Input Validation: When the data for a new speaker is entered, be sure the user enters
+//  data for all the fields. No negative amounts should be entered for a speaker's fee.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
     return 0;
 }
